1880c0388d45a46b7bc4136a6c1e8141e62e0d09
rieckypoerwadiredja <rieckysyarief@gmail.com>
2023-02-28 16:14:02 +0700

    Make CORS porxy for localhost and vercel

--- server/server.js
+++ server/server.js
@@ -1,12 +1,17 @@
-const express = require('express');
+const express = require("express");
+const cors = require("cors");
 
 const app = express();
-
+var corsOptions = {
+  origin: ["https://vercel.com/", "http://localhost:3000"],
+  optionsSuccessStatus: 200, // For legacy browser support
+};
+app.use(cors());
 // Controller
-const portofolio = require('./controllers/portfolio');
-const notFound = require('./controllers/notFound');
+const portofolio = require("./controllers/portfolio");
+const notFound = require("./controllers/notFound");
 
-app.get('/api/porto', portofolio.getPorto);
+app.get("/api/porto", portofolio.getPorto);
 
 app.use(notFound.notFound);
 
