6f2bb4ecaca60bacbeb5e7afcc77a90309b5ee9f
rieckypoerwadiredja <rieckysyarief@gmail.com>
2023-04-15 17:46:04 +0700

    Update: Copyright footer section

--- client/src/component/Footer/Copyright.jsx
+++ client/src/component/Footer/Copyright.jsx
@@ -1,12 +1,35 @@
 import React from "react";
-
-
-function Copyright() {
-    return(
-        <div>
-            Copyright
-        </div>
-    )
+import PropTypes from "prop-types";
+function Copyright({ socialMedia }) {
+  const year = new Date().getFullYear();
+  return (
+    <div className="flex justify-between py-8 px-5 bg-blackFont-800 text-trans-white-800 border-t-[1px] border-trans-white-400">
+      <p className="text-[14px] xlX:text-3xl">
+        {`Virtual Technology — ©${year} All rights reserved`}
+      </p>
+      <p className="text-[14px] xlX:text-3xl">
+        Check our Social Media —
+        {socialMedia.map((social, idx) => (
+          <>
+            {" "}
+            <a
+              className="underline text-white"
+              href={social.link}
+              target="_blank"
+              rel="noreferrer"
+              key={idx}
+            >
+              {social.name}
+            </a>
+            {idx !== socialMedia.length - 1 && <span className="mx-1">|</span>}
+          </>
+        ))}
+      </p>
+    </div>
+  );
 }
 
+Copyright.propTypes = {
+  socialMedia: PropTypes.arrayOf(PropTypes.object).isRequired,
+};
 export default Copyright;
--- client/src/component/Footer/Footer.jsx
+++ client/src/component/Footer/Footer.jsx
@@ -5,14 +5,18 @@ import ProfileWithCompany from "../Profile/ProfileWithCompany";
 import SectionMainFooter from "../Section/SectionMainFooter";
 // Context
 import FooterContext from "../../context/FooterContext";
+import Copyright from "./Copyright";
 
 function Footer() {
-  const { img, title, desc, contact } = useContext(FooterContext);
+  const { img, title, desc, contact, social } = useContext(FooterContext);
   return (
-    <div className="pt-[50px] smXL:pt-[80px] xlX:py-[90px] px-web-sm smXL:px-web-md xlX:px-web-lg bg-blackFont-800 text-white">
-      <ProfileWithCompany src={img.src} alt={img.alt} />
-      <SectionMainFooter title={title} desc={desc} button={contact} />
-    </div>
+    <>
+      <div className="pt-[50px] smXL:pt-[80px] xlX:py-[90px] px-web-sm smXL:px-web-md xlX:px-web-lg bg-blackFont-800 text-white">
+        <ProfileWithCompany src={img.src} alt={img.alt} />
+        <SectionMainFooter title={title} desc={desc} button={contact} />
+      </div>
+      <Copyright socialMedia={social} />
+    </>
   );
 }
 
