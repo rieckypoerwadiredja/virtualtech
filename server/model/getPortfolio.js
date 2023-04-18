36b42b9445afa25db410e8848484cc67e248af77
rieckypoerwadiredja <rieckysyarief@gmail.com>
2023-04-18 15:25:58 +0700

    Update: Porto Data

--- server/model/getPortfolio.js
+++ server/model/getPortfolio.js
@@ -1,4 +1,33 @@
-const portofolio = ["user1", "user2", "user3"];
+const portofolio = [
+  {
+    id: 1,
+    type: "Landscapes",
+    title:
+      "A collection of the most interesting photos and galleries taken during the flights.",
+    creator: {
+      name: "Andreas Moris",
+      desc: "Blog Director",
+      image: "./drone.jpg",
+      alt: "drone",
+    },
+    cardImage: "",
+    background: null,
+  },
+  {
+    id: 2,
+    type: "Tutorials",
+    title:
+      "Top 10 Best equipment for beginners. Selection and advice when buying drone.",
+    creator: {
+      name: "John Moris",
+      desc: "We created diagrams and analyzes",
+      image: "./drone.jpg",
+      alt: "drone",
+    },
+    cardImage: null,
+    background: "",
+  },
+];
 
 // eslint-disable-next-line arrow-body-style
 const getPortfolio = async () => {
