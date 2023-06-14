7d1551caf85f0d36fd0a49990b48519b6c34b058
rieckypoerwadiredja <rieckysyarief@gmail.com>
2023-06-14 17:58:44 +0700

    Update: Vercel Analytics

--- client/src/App.js
+++ client/src/App.js
@@ -12,7 +12,8 @@ import "./input.css";
 import PortosContext from "./context/PortosContext.js";
 import AboutUs from "./pages/AboutUs.jsx";
 import ErrorPage from "./pages/ErrorPage.jsx";
-
+// Vercel Analytic
+import { Analytics } from "@vercel/analytics/react";
 if (process.env.NODE_ENV !== "development") {
   console.error = (message) => {
     throw new Error(message);
@@ -21,30 +22,36 @@ if (process.env.NODE_ENV !== "development") {
 
 function App() {
   return (
-    <div className="App max-w-[100%] overflow-hidden">
-      <PortosContext>
-        <Routes>
-          <Route path="/" element={<HomePage />} />
-          <Route path="/portfolio">
-            <Route index element={<Portfolio />} />
-            <Route path=":creator/:id/:title" element={<DetailPortofolio />} />
-          </Route>
-          <Route path="/aboutus" element={<AboutUs />} />
-          <Route path="/contact" element={<Contact />} />
-          <Route path="/privacy-policy" element={<PrivacyPolicy />} />
-          <Route
-            path="*"
-            element={
-              <ErrorPage
-                errorCode={404}
-                status="Not Found"
-                message="The requested resource was not found on the server."
+    <>
+      <div className="App max-w-[100%] overflow-hidden">
+        <PortosContext>
+          <Routes>
+            <Route path="/" element={<HomePage />} />
+            <Route path="/portfolio">
+              <Route index element={<Portfolio />} />
+              <Route
+                path=":creator/:id/:title"
+                element={<DetailPortofolio />}
               />
-            }
-          />
-        </Routes>
-      </PortosContext>
-    </div>
+            </Route>
+            <Route path="/aboutus" element={<AboutUs />} />
+            <Route path="/contact" element={<Contact />} />
+            <Route path="/privacy-policy" element={<PrivacyPolicy />} />
+            <Route
+              path="*"
+              element={
+                <ErrorPage
+                  errorCode={404}
+                  status="Not Found"
+                  message="The requested resource was not found on the server."
+                />
+              }
+            />
+          </Routes>
+        </PortosContext>
+      </div>
+      <Analytics />
+    </>
   );
 }
 
